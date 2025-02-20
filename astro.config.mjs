53cdbde201b15f8b70b18f20d09054273f8db4be
unknown <dariorodq30@gmail.com>
2025-02-20 18:06:07 -0500

    workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://dariorodro.github.io/mip0rtaf0li0',
+    base: 'mip0rtaf0li0',
+});
