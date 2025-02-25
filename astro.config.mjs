6dd83e19111677e84199951bea1808a237a3ca7e
unknown <dariorodq30@gmail.com>
2025-02-25 18:17:57 -0500

    xd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://dariorodro.github.io/mip0rtaf0li0',
-    base: 'mip0rtaf0li0',
+    base: '/mip0rtaf0li0',
 });
